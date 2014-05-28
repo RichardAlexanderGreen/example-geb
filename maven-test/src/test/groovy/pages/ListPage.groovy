38b3b9520a4bcd5c660b43ac71b5727c14c48b92
Terry Weymouth <weymouth@umich.edu>
2014-05-27 23:15:10 -0400

    Slide set (first version) and minor changes to code (refactoring)

--- maven-test/src/test/groovy/pages/ListPage.groovy
+++ maven-test/src/test/groovy/pages/ListPage.groovy
@@ -1,6 +1,6 @@
 package pages
 
-import geb.Module
+import modules.PersonRow
 
 class ListPage extends ScaffoldPage {
 	static url = "person/list"
@@ -14,17 +14,5 @@ class ListPage extends ScaffoldPage {
 		peopleTable { $("div.content table", 0) }
 		personRow { module PersonRow, personRows[it] }
 		personRows(required: false) { peopleTable.find("tbody").find("tr") }
-	}
-}
-
-class PersonRow extends Module {
-	static content = {
-		cell { $("td", it) }
-		cellText { cell(it).text() }
-        cellHrefText{ cell(it).find('a').text() }
-		enabled { Boolean.valueOf(cellHrefText(0)) }
-		firstName { cellText(1) }
-		lastName { cellText(2) }
-		showLink(to: ShowPage) { cell(0).find("a") }
 	}
 }
